85c4b473c217652837b0fcfe83e38cbc7a98f804
Huy Vo <voxqhuy@Huys-MacBook-Pro.local>
2019-12-28 16:44:13 -0500

    add github

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
-    
+    GitHubCalendar("#github-graph", "kohnewlife", { responsive: true });
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
