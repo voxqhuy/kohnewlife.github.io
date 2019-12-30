f947c1de9caaf50d08c9d91ab418743ae18bf91e
Huy Vo <voxqhuy@Huys-MacBook-Pro.local>
2019-12-30 00:25:36 -0500

    update events, blog spots, other projects, work experience

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "https://feeds.feedburner.com/TechCrunch/startups",
+        "https://medium.com/feed/@voxqhuy",
         
         {
         // how many entries do you want?
